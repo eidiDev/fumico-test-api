ae30cbe76d8e1e1a94d9e71cdeae00d3c3bb0625
EidiKumagai <lucaseidikumagai@gmail.com>
2022-07-26 22:53:26 -0300

    Ajustes no DTO de reminders:  update com class validator opcional

--- src/modules/reminders/dto/create-reminder.dto.ts
+++ src/modules/reminders/dto/create-reminder.dto.ts
@@ -1,14 +1,15 @@
-import { IsString, IsObject} from 'class-validator';
+import { IsString, IsObject, IsOptional} from 'class-validator';
 import { BaseEntity } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 
 export class CreateReminderDto extends BaseEntity {
+    
     @IsString()
-    readonly title?:string;
+    readonly title:string;
 
     @IsString()
-    readonly description?:string;
+    readonly description:string;
 
     @IsObject()
-    readonly user?: User
+    readonly user: User
 }
